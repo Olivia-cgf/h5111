bee1b5fd059085ee35808cf4ddee557c2168fee8
chengguifang <chengguifang@aactechnologies.com>
2018-07-27 16:26:38 +0800

    first master

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(function(){
+    $(".company, .research, .introduce").hide();
+    $(".homebtn").click(function () {
+        $(".company, .research").show();
+        // $(".company").slideDown();
+        // $(".home").anmiate({'top',})
+    });
+    // $(".research").click(function(){
+    //     $(".introduce").fadeIn();
+    // })
+    
+})
