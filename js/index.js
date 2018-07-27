bbaa67b3f5b7c30702dcb508d6854e7998d31fee
MacBook Pro <macbookpro@MacBookdeMacBook-Pro.local>
2018-07-27 23:32:30 +0800

    second first

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
 $(function(){
-    $(".company, .research, .introduce").hide();
+    console.log($(document).width());
+    var clientWidth = $(document).width();
+    var clientHeight = $(window).height();
+    console.log(clientHeight);
+    $(".company, .position, .introduce").hide();
     $(".homebtn").click(function () {
         $(".company, .research").show();
+        $('body,html').animate({'scrollTop':clientHeight},3000);
         // $(".company").slideDown();
         // $(".home").anmiate({'top',})
     });
     // $(".research").click(function(){
     //     $(".introduce").fadeIn();
     // })
-    
+    $(".position").click(function(){
+        $(".inner-wrapper").css("width", clientWidth*2);
+        $(".position").css("width", clientWidth);
+        
+      
+        $(this).prev().animate(function(){
+            $(this).prev().show();
+            $(this).prev().css("width",clientWidth)
+        },3000);
+    })
 })
