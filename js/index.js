6d60c4f1c0bbc422c0e4cd9428417adc222cd02b
MacBook Pro <macbookpro@MacBookdeMacBook-Pro.local>
2018-07-29 22:52:09 +0800

    first commit

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(function(){
     var clientWidth = $(document).width();
     var clientHeight = $(window).height();
     console.log(clientHeight);
-    $(".company, .position, .introduce").hide();
+    $(".company, .introduce").hide();
     $(".homebtn").click(function () {
         $(".company, .research").show();
         $('body,html').animate({'scrollTop':clientHeight},3000);
@@ -13,14 +13,14 @@ $(function(){
     // $(".research").click(function(){
     //     $(".introduce").fadeIn();
     // })
-    $(".position").click(function(){
-        $(".inner-wrapper").css("width", clientWidth*2);
-        $(".position").css("width", clientWidth);
+    // $(".position").click(function(){
+    //     $(".inner-wrapper").css("width", clientWidth*2);
+    //     $(".position").css("width", clientWidth);
         
       
-        $(this).prev().animate(function(){
-            $(this).prev().show();
-            $(this).prev().css("width",clientWidth)
-        },3000);
-    })
+    //     $(this).prev().animate(function(){
+    //         $(this).prev().show();
+    //         $(this).prev().css("width",clientWidth)
+    //     },3000);
+    // })
 })
